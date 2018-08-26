158ca07402b2f8b9f19c855a34abb5aaa52be3cc
Robiel <robielasgedom@gmail.com>
2018-08-25 23:45:11 -0400

    1

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+var friends = [
+    {
+        name: "Chantel",
+        photo: "https://images.pexels.com/photos/1068207/pexels-photo-1068207.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores: [
+            1,
+            1, 
+            3, 
+            4, 
+            5, 
+            4, 
+            5, 
+            3, 
+            2, 
+            5
+        ]
+    },
+    {
+        name: "Leah",
+        photo: "https://images.pexels.com/photos/1181521/pexels-photo-1181521.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores: [
+            4, 
+            2, 
+            3, 
+            4, 
+            5, 
+            2, 
+            1, 
+            2, 
+            4, 
+            3
+        ]
+    },
+    {
+        name: "James",
+        photo: "https://images.pexels.com/photos/1181391/pexels-photo-1181391.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores: [
+            5, 
+            1, 
+            4, 
+            5, 
+            4, 
+            1, 
+            2, 
+            3, 
+            1, 
+            5
+        ]
+    },
+    {
+        name: "Keith",
+        photo: "https://images.pexels.com/photos/1170974/pexels-photo-1170974.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores: [
+            3, 
+            4, 
+            3,
+            4, 
+            5, 
+            5, 
+            3, 
+            1, 
+            2, 
+            1
+        ]
+    },
+    {
+        name: "Frank",
+        photo: "https://images.pexels.com/photos/935946/pexels-photo-935946.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores: [
+            2, 
+            2, 
+            3, 
+            4,
+            5, 
+            4, 
+            7, 
+            2, 
+            2, 
+            3
+        ]
+    },
+    
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,54 @@
+
+var friendData = require("../data/friends.js");
+function display(name, photo){
+	name: name;
+	photo: photo;
+}
+module.exports = function(app){
+	app.get("/api/friends", function(req,res){
+		res.json(friendData);
+	});
+	app.post("/api/friends", function(req,res){
+		// friendData.push({
+		// 	name: req.body.name,
+		// 	photo: req.body.photo,
+		// 	scores: req.body.scores
+		// });
+		var diffs = [];
+		var num = 0;
+		var totaldif = [];
+		var total = 0;
+		var data;
+		friendData.forEach(function(element){
+			var user1 = element.scores;
+			var user2 = req.body.scores;
+			for (var i = 0; i < element.scores.length; i++){
+				var num = parseInt(user1[i])-parseInt(user2[i]);
+				diffs.push(Math.abs(num));
+			}
+			console.log(diffs);
+			for(var i = 0; i <diffs.length; i++){
+				total =  parseInt(diffs[i])+parseInt(total);
+			}
+			totaldif.push(total);
+			total = 0;
+			diffs = [];
+		});
+		Array.min= function(array){
+			return Math.min.apply(Math, array);
+		}
+		var match = Array.min(totaldif)
+		for (var i =0; i<totaldif.length; i++){
+			if 	(totaldif[i] == match) {
+				data = friendData[i]
+				res.send(data);
+			}else{
+				console.log("not a match")
+			}
+		}
+		console.log(totaldif);
+		;
+		res.json(true)
+
+	});
+};
