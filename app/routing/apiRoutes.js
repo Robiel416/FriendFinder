d86fd6a55c532a4a8f25ac7d34889df260a182cf
Robiel <robielasgedom@gmail.com>
2018-08-28 18:30:09 -0400

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@ var friends = require("../data/friends.js");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res){
-        res.join(friends);
+       return res.join(friends);
     });
 
     app.post("/api/friends", function(req, res) {
